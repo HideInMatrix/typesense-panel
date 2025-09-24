0381476987f99f05bc440cb40b3aacbb60fa2dd6
HideInMatrix <davidmorgancreate@gmail.com>
2025-09-24 21:20:19 +0800

    test: 调试edgeone

--- server/api/login.post.ts
+++ server/api/login.post.ts
@@ -1,27 +1,25 @@
-import { z } from 'zod'
+import { z } from "zod";
 
 const bodySchema = z.object({
   email: z.string().email(),
-  password: z.string().min(8)
-})
+  password: z.string().min(8),
+});
 
 export default defineEventHandler(async (event) => {
-  const { email, password } = await readValidatedBody(event, bodySchema.parse)
+  const { email, password } = await readValidatedBody(event, bodySchema.parse);
 
-   console.log(email,password);
-   
-  if (email === 'test@qq.com' && password === '12345678') {
+  if (email === "test@qq.com" && password === "12345678") {
     // set the user session in the cookie
     // this server util is auto-imported by the auth-utils module
     await setUserSession(event, {
       user: {
-        name: 'John Doe'
-      }
-    })
-    return {}
+        name: "管理员",
+      },
+    });
+    return {};
   }
   throw createError({
     statusCode: 401,
-    message: 'Bad credentials'
-  })
-})
+    message: "Bad credentials",
+  });
+});
